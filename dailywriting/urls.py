6520133b0813a500d01e7905612a3c65ac58c0de
Dwight Gunning <dwight@dwightgunning.com>
2017-10-22 10:57:50 +0200

    Added 'append slash' url redirects to base admin and api urls

--- dailywriting/urls.py
+++ dailywriting/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
+    url(r'^api$', RedirectView.as_view(url='/api/')),
     url(r'^admin/', admin.site.urls),
+    url(r'^admin$', RedirectView.as_view(url='/admin/')),
     # Whitenoise cannot serve a static index for the website root root URL.
     # See also settings.TEMPLATES
     url(r'^.*$', TemplateView.as_view(template_name='index.html'))
