c45048a505c7dc34b673afdb9eeac90dbb1c6bb6
Dwight Gunning <dwight@dwightgunning.com>
2018-03-26 11:01:17 +1000

    Fixed failing test

--- ng-src/app/review-page/review-page.component.spec.ts
+++ ng-src/app/review-page/review-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ReviewPageComponent } from '../review-page/review-page.component';
 
@@ -16,7 +17,9 @@ describe('ReviewPageComponent', () => {
         StubEntryListComponent,
         ReviewPageComponent,
       ],
-      imports: [],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
       providers: []
     })
     .compileComponents();
